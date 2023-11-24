86451fb98a9ae0a4c64ff5daebfa7fe1b8fd68c1
SzymoneX300 <szymonwroblewski74@gmail.com>
2023-11-24 18:45:54 +0100

    increased the distance check interval and fixed the vertical FOV

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
 public class Main {
     /**
-     * To wiev this properly, you need to run it in CMD
+     * To view this properly, you need to run it in CMD, set in
+     * CMD's properties > layout > windows size > width to SCREEN_WIDTH
+     * and height to SCREEN_HEIGHT and in font tab, set a font that's
+     * height is 1.5 it's width
+     *
+     * (ie I use 'RasterFonts' with size 4x6, that's the smallest size font
+     * that doesn't produce colorbleed effect at least on my screen and
+     * displays proper shades of gray, other sometimes tint the image
+     * purple or brown from my testing)
      */
 
-    private static char[] map = {
+    private static final char[] MAP = {
             '#','#','#','#','#','#','#','#','#','#',
             '#',' ','#',' ','#',' ',' ',' ','#','#',
-            '#',' ',' ',' ','#','#','#',' ','#','#',
-            '#','#',' ','#',' ','#','#',' ','#','#',
-            '#',' ',' ','#',' ',' ','#',' ','#','#',
-            '#',' ','#','#','#','#','#',' ','#','#',
-            '#',' ','#',' ',' ',' ','#',' ','#','#',
-            '#',' ','#',' ','#',' ','#',' ','#','#',
-            '#',' ',' ',' ','#',' ','#',' ','#','#',
+            '#',' ',' ',' ','#','#','#',' ',' ','#',
+            '#','#',' ',' ','#',' ',' ',' ',' ','#',
+            '#',' ','#',' ',' ',' ','#',' ',' ','#',
+            '#',' ','#','#','#',' ',' ',' ',' ','#',
+            '#',' ','#',' ',' ',' ','#',' ',' ','#',
+            '#',' ','#',' ','#',' ','#',' ',' ','#',
+            '#',' ',' ',' ','#',' ','#',' ',' ','#',
             '#','#','#','#','#','#','#','#','#','#'
     };
     private static final long FRAME_TIME = 1_000_000_000 / 60;
     private static final int SCREEN_WIDTH = 479, SCREEN_HEIGHT = 179;
-    private static final double SCREEN_FOV = Math.PI * (70.0 / 180);
-    private static double playerX = 7.5, playerY = 1.5, playerA = 0;
+    private static final double SCREEN_FOV = Math.PI * (60.0 / 180);
+    private static double playerX = 7.5, playerY = 3.5, playerA = 0;
     private static final WriteCMDBuffer CMD = new WriteCMDBuffer(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    private static boolean run = true;
+
     private static void drawScreen(){
-        double start = System.nanoTime(), end = 0;
         char[][] tempScreen = new char[SCREEN_WIDTH][SCREEN_HEIGHT];
         for (int i = 0; i < SCREEN_WIDTH; i++) {
             double rayAngle = (playerA + SCREEN_FOV / 2) - ((double) i / SCREEN_WIDTH ) * SCREEN_FOV;
@@ -31,24 +40,22 @@ public class Main {
 
             double eyeX = Math.sin(rayAngle), eyeY = Math.cos(rayAngle);
 
-            while(!hit && dist < 9){
-                dist += 0.0025;
+            while(!hit && dist < 5){
+                dist += 0.005;
 
                 int testX = (int)(playerX + eyeX * dist);
                 int testY = (int)(playerY + eyeY * dist);
 
                 if(testX < 0 || testX > 10 || testY < 0 || testY > 10){
                     hit = true;
-                    dist = 9;
+                    dist = 5;
                 } else{
-                    if(map[(testY * 10) + testX] == '#'){
+                    if(MAP[(testY * 10) + testX] == '#'){
                         hit = true;
                     }
                 }
             }
-            //int wallHeight = (int) (Math.round(((9-dist)/9) * 176) + 1);
-            //int halfSpace  = (int) Math.floor((double)(SCREEN_HEIGHT - wallHeight) / 2) + 1;
-            int halfSpace = (int) ((((double) SCREEN_HEIGHT) / 2) - (((double) SCREEN_HEIGHT) * dist)); //create excel to see how the height changes!!!!!!!!!!!!!!!!
+            int halfSpace = (dist != 0)?(int) Math.min(Math.floor(((double) SCREEN_HEIGHT / 2) - ((SCREEN_HEIGHT / dist) / 1.7)), Math.floor((double) SCREEN_HEIGHT / 2)) : -1;
             int wallHeight = SCREEN_HEIGHT - (2 * halfSpace);
             wallHeight += SCREEN_HEIGHT - (wallHeight + (2 * halfSpace));
             for (int j = 0; j < SCREEN_HEIGHT; j++) {
@@ -71,13 +78,6 @@ public class Main {
                 }
             }
         }
-        for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 12; j++) {
-                if(i == 0 || i == 11 || j == 0 || j == 11) tempScreen[j][i] = ' ';
-                else tempScreen[j][i] = map[((i-1)*10)+j-1];
-            }
-        }
-        tempScreen[((int) playerX)+1][((int) playerY)+1] = '@';
         for (int i = 0; i < SCREEN_HEIGHT; i++) {
             for (int j = 0; j < SCREEN_WIDTH; j++) {
                 CMD.screen[(i*SCREEN_WIDTH)+j] = tempScreen[j][i];
@@ -85,15 +85,16 @@ public class Main {
         }
         CMD.writeScreen();
         playerA -= Math.PI / 480;
-        if(playerA >= Math.PI * 2) playerA -= Math.PI * 2;
-        do{
-            end = System.nanoTime();
-        }while(start + FRAME_TIME > end);
     }
 
     public static void main(String[] args) {
-        while(true){
+        while(run){
+            double start = System.nanoTime(), end = 0;
             drawScreen();
+            if(playerA >= Math.PI * 2) playerA -= Math.PI * 2;
+            do{
+                end = System.nanoTime();
+            }while(start + FRAME_TIME > end);
         }
     }
 }
