fd3051b13038831fab3ae0bf6640de1937695a1e
SzymoneX300 <szymonwroblewski74@gmail.com>
2023-12-01 02:20:00 +0100

    removed collisions, tried rewriting rendering algorithm for better performance but still buggy

--- src/Main.java
+++ src/Main.java
@@ -26,9 +26,9 @@ public class Main{
             '#',' ',' ',' ','#',' ','#',' ',' ','#',
             '#','#','#','#','#','#','#','#','#','#'
     };
-    private static final long FRAME_TIME = 1_000_000_000 / 60;
+    private static final long FRAME_TIME = 1_000_000_000 / 240;
     private static final int SCREEN_WIDTH = 479, SCREEN_HEIGHT = 179;
-    private static final double SCREEN_FOV = Math.PI * (60.0 / 180), PLAYER_SPEED = 0.02;
+    private static final double SCREEN_FOV = Math.PI * (90.0 / 180), PLAYER_SPEED = 0.005;
     private static final WriteCMDBuffer CMD = new WriteCMDBuffer(SCREEN_WIDTH, SCREEN_HEIGHT);
     private static double playerX = 7.5, playerY = 3.5, playerA = 0;
     private static boolean isMovingForward = false, isMovingBackward = false, isMovingLeft = false, isMovingRight = false, isRotatingLeft = false, isRotatingRight = false, run = true;
@@ -36,13 +36,82 @@ public class Main{
     private static void drawScreen(){
         char[][] tempScreen = new char[SCREEN_WIDTH][SCREEN_HEIGHT];
         for (int i = 0; i < SCREEN_WIDTH; i++) {
-            double rayAngle = (playerA + SCREEN_FOV / 2) - ((double) i / SCREEN_WIDTH ) * SCREEN_FOV;
-            double dist = 0;
-            boolean hit = false;
+            double rayAngle = (playerA + SCREEN_FOV / 2) - ((double) i / SCREEN_WIDTH ) * SCREEN_FOV, rayX, rayY, offsetX = 0, offsetY = 0, dist = 0, horizontalDist = 0, verticalDist = 0;
 
-            double eyeX = Math.sin(rayAngle), eyeY = Math.cos(rayAngle);
 
-            while(!hit && dist < 4.8){
+            //Checking horizontal lines for collisions
+            double angleTan = -1 / Math.tan(rayAngle);
+
+            //Looking north
+            if(rayAngle > Math.PI / 2 && rayAngle < (3 * Math.PI) / 2) {
+                rayY = Math.floor(playerY) - 0.0001;
+                rayX = (playerY - rayY) * angleTan + playerX;
+                offsetY = -1;
+                offsetX = offsetY * -1 * angleTan;
+            }
+            //Looking south
+            else if(rayAngle < Math.PI / 2 || rayAngle > (3 * Math.PI) / 2) {
+                rayY = Math.floor(playerY) + 1;
+                rayX = (playerY - rayY) * angleTan + playerX;
+                offsetY = 1;
+                offsetX = offsetY * -1 * angleTan;
+            }
+            //Looking straigth east or west
+            else{
+                rayX = playerX;
+                rayY = playerY;
+                horizontalDist = 5;
+            }
+
+            //Iterating untill ray hits a wall or distance gets over 5
+            while (horizontalDist < 5){
+                int mapIndex = (((int)rayY) * 10) + ((int)rayX);
+                horizontalDist = Math.sqrt((rayX * rayX) + (rayY * rayY));
+                if(mapIndex < MAP.length && mapIndex >= 0 && MAP[mapIndex] == '#')break;
+                else{
+                    rayX += offsetX;
+                    rayY += offsetY;
+                }
+            }
+
+
+            //Checking vertical lines for collisions
+            angleTan = -1 * Math.tan(rayAngle);
+
+            //Looking west
+            if(rayAngle > Math.PI  && rayAngle < 2 * Math.PI) {
+                rayX = Math.floor(playerX - 0.0001);
+                rayY = (playerX - rayX) * angleTan + playerY;
+                offsetX = -1;
+                offsetY = offsetX * -1 * angleTan;
+            }
+            //Looking east
+            else if(rayAngle < Math.PI || rayAngle > 0) {
+                rayX = Math.floor(playerX) + 1;
+                rayY = (playerX - rayX) * angleTan + playerY;
+                offsetX = 1;
+                offsetY = offsetX * -1 * angleTan;
+            }
+            //Loogind straight north or south
+            else{
+                rayX = playerX;
+                rayY = playerY;
+                verticalDist = 5;
+            }
+            //Iterating untill ray hits a wall or distance gets over 5
+            while (verticalDist < 5){
+                int mapIndex = (((int)rayY) * 10) + ((int)rayX);
+                verticalDist = Math.sqrt((rayX * rayX) + (rayY * rayY));
+                if(mapIndex < MAP.length && mapIndex >= 0 && MAP[mapIndex] == '#')break;
+                else{
+                    rayX += offsetX;
+                    rayY += offsetY;
+                }
+            }
+
+            dist = Math.min(verticalDist, horizontalDist);
+
+            /*while(!hit && dist < 4.8){
                 dist += 0.005;
 
                 int testX = (int)(playerX + eyeX * dist);
@@ -56,10 +125,12 @@ public class Main{
                         hit = true;
                     }
                 }
-            }
-            int halfSpace = (dist != 0)?(int) Math.min(Math.floor(((double) SCREEN_HEIGHT / 2) - ((SCREEN_HEIGHT / dist) / 1.7)), Math.floor((double) SCREEN_HEIGHT / 2)) : -1;
-            int wallHeight = SCREEN_HEIGHT - (2 * halfSpace);
-            wallHeight += SCREEN_HEIGHT - (wallHeight + (2 * halfSpace));
+            }*/
+            double playerRayAngleDiff = Math.abs(rayAngle - playerA) * -1;
+            playerRayAngleDiff += (playerRayAngleDiff < 0) ? 2 * Math.PI : ((playerRayAngleDiff >= 2* Math.PI) ? ((-2) * Math.PI) : 1);
+            int wallHeight = Math.min((int)((SCREEN_HEIGHT)/(dist * Math.cos(playerRayAngleDiff))),SCREEN_HEIGHT);
+            int halfSpace = (int) Math.floor(((double)(SCREEN_HEIGHT - wallHeight)) / 2);
+            if(wallHeight < SCREEN_HEIGHT) wallHeight += SCREEN_HEIGHT - (wallHeight + (2 * halfSpace));
 
             for (int j = 0; j < SCREEN_HEIGHT; j++) {
                 if(j < halfSpace) tempScreen[i][j] = ' ';
@@ -89,11 +160,11 @@ public class Main{
                         else if (dist < 3.64)   tempScreen[i][j] = '¦';                             //'¦¦¦¦¦¦'
                         else                    tempScreen[i][j] = ((i + j) % 2 == 0) ? '¦' : ':';  //'¦:¦:¦:'
                     }
-                    else if (dist < 3.92)   tempScreen[i][j] = ':';                             //'::::::'
-                    else if (dist < 4)      tempScreen[i][j] = ((i + j) % 2 == 0) ? ':' : '.';  //':.:.:.'
-                    else if (dist < 4.44)   tempScreen[i][j] = '.';                             //'......'
-                    else if (dist < 4.8)    tempScreen[i][j] = ((i + j) % 2 == 0) ? '.' : ' ';  //'. . . '
-                    else                    tempScreen[i][j] = ' ';                             //'      '
+                    else if (dist < 3.92)   tempScreen[i][j] = ':';                                 //'::::::'
+                    else if (dist < 4)      tempScreen[i][j] = ((i + j) % 2 == 0) ? ':' : '.';      //':.:.:.'
+                    else if (dist < 4.44)   tempScreen[i][j] = '.';                                 //'......'
+                    else if (dist < 4.8)    tempScreen[i][j] = ((i + j) % 2 == 0) ? '.' : ' ';      //'. . . '
+                    else                    tempScreen[i][j] = ' ';                                 //'      '
 
                 }
             }
@@ -123,8 +194,8 @@ public class Main{
 
     public static void calculateMovement(){
         double tempPlayerX = playerX, tempPlayerY = playerY;
-        if(isRotatingRight && !isRotatingLeft) playerA -= Math.PI / 240;
-        else if(isRotatingLeft) playerA += Math.PI / 240;
+        if(isRotatingRight && !isRotatingLeft) playerA -= Math.PI / 480;
+        else if(isRotatingLeft) playerA += Math.PI / 480;
 
         if(playerA >= Math.PI * 2) playerA -= Math.PI * 2;
         else if (playerA < 0) playerA = (Math.PI *2) + playerA;
@@ -149,7 +220,8 @@ public class Main{
         tempPlayerX = Math.max(1.1,Math.min(8.9, tempPlayerX));
         tempPlayerY = Math.max(1.1,Math.min(8.9, tempPlayerY));
 
-        if(Math.abs(tempPlayerY - playerY) > Math.abs(tempPlayerX - playerX)){
+        /**buggy wall collision code
+        /*if(Math.abs(tempPlayerY - playerY) > Math.abs(tempPlayerX - playerX)){
             if (tempPlayerX < playerX && (MAP[(((int) (tempPlayerY - 0.1)) * 10) + (int) (tempPlayerX - 0.1)] == '#' || MAP[(((int) (tempPlayerY + 0.1)) * 10) + (int) (tempPlayerX - 0.1)] == '#'))
                 tempPlayerX = ((int) (tempPlayerX - 0.1)) + 1.1;
             else if (tempPlayerX > playerX && (MAP[(((int) (tempPlayerY - 0.1)) * 10) + (int) (tempPlayerX + 0.1)] == '#' || MAP[(((int) (tempPlayerY + 0.1)) * 10) + (int) (tempPlayerX + 0.1)] == '#'))
@@ -160,7 +232,7 @@ public class Main{
                 tempPlayerY = ((int) (tempPlayerY - 0.1)) + 1.1;
             else if (tempPlayerY > playerY && (MAP[(((int) (tempPlayerY - 0.1)) * 10) + (int) (tempPlayerX + 0.1)] == '#' || MAP[(((int) (tempPlayerY - 0.1)) * 10) + (int) (tempPlayerX - 0.1)] == '#'))
                 tempPlayerY = ((int) (tempPlayerY + 0.1)) - 0.1;
-        }
+        }*/
 
         playerX = tempPlayerX;
         playerY = tempPlayerY;
